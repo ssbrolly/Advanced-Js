3065b214a3fa15aab246ba9abf1adde1259a6625
Usong Cho <silvermouse4012@gmail.com>
2018-07-20 19:53:12 -0700

    minor

--- script.js
+++ script.js
@@ -15,26 +15,57 @@
 //     // };
 // };
 
+// function Person(name, birthYear, job) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//     this.job = job;
+// }
+
+// Person.prototype.calcAge = function() {
+//     console.log(2018 - this.birthYear); 
+// };
+
+// let john = new Person("Jon", 1979, "teacher");
+// let jane = new Person("Jane", 1996, "Designer");
+// let mark = new Person("Mark", 1975, "SalesPerson");
+
+// Person.prototype.lastName = "Smith";
+
+// john.fullName = function() {
+//     console.log(this.name + " " + this.lastName);
+// };
+// john.fullName();
+// john.calcAge();
+// console.log(john.name + john.lastName);
+// ;
+
+// Object.create
+
+let personProto = {
+    calcAge: function() {
+        console.log(2018 - this.birthYear);
+    }
+};
+
+let john = Object.create(personProto);
+john.name = "John";
+john.birthYear = 1990;
+john.job = "teacher";
+
+let jane = Object.create(personProto, {
+    name: {value: "Jane"},
+    birthYear: {value: 1992},
+    job: {value: "Designer"}
+});
+
 function Person(name, birthYear, job) {
     this.name = name;
     this.birthYear = birthYear;
     this.job = job;
+    this.age = function() {
+        return 2018 - this.birthYear;
+    }
 }
 
-Person.prototype.calcAge = function() {
-    console.log(2018 - this.birthYear); 
-};
-
-
-let john = new Person("Jon", 1979, "teacher");
-let jane = new Person("Jane", 1996, "Designer");
-let mark = new Person("Mark", 1975, "SalesPerson");
-
-john.lastName = "Smith";
-john.fullName = function() {
-    console.log(this.name + " " + this.lastName);
-};
-john.fullName();
-john.calcAge();
-console.log(john.name + john.lastName);
-;
+let dan = new Person("jon", 1993, "driver");
+console.log(dan.age());
